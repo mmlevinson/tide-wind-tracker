1de2a1136936481b65cd582cba2eddc4588a19eb
Cheechako <mmlevinson@gmail.om>
2026-08-10 08:16:55 -0700

    BIYcoder: published v0.1 to production.

---
+++ src/constants/siteVersion.ts
@@ -0,0 +1,5 @@
+// Auto-updated by BIYcoder on publish — do not edit manually.
+export const VersionMajor = 0
+export const VersionMinor = 1
+export const SiteVersion = "0.1"
+export const SiteVersionSemVer = "0.1.0"
