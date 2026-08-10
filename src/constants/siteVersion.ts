f74e00a47078422878d7c4e341c613544d9d135e
Cheechako <mmlevinson@gmail.om>
2026-08-10 13:46:16 -0700

    BIYcoder: release v0.3.

--- src/constants/siteVersion.ts
+++ src/constants/siteVersion.ts
@@ -1,5 +1,5 @@
 // Auto-updated by BIYcoder on publish — do not edit manually.
 export const VersionMajor = 0
-export const VersionMinor = 2
-export const SiteVersion = "0.2"
-export const SiteVersionSemVer = "0.2.0"
+export const VersionMinor = 3
+export const SiteVersion = "0.3"
+export const SiteVersionSemVer = "0.3.0"
